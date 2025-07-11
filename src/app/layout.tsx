caf5ecd7ee2e08220c671a88b36b336116702778
Prit Patel <pritp@solguruz.com>
2025-07-11 21:51:32 +0530

    fix:hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body
+        cz-shortcut-listen="true"
+      >
         {children}
       </body>
     </html>
